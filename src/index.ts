1a805fb80291daeaa445b02105eb2801bb1a8777
Mnishek <shavyrina.marina@mail.ru>
2022-02-04 18:35:57 +0300

    dz_1

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { renderSearchFormBlock } from './search-form.js'
+import { renderSearchStubBlock } from './search-results.js'
+import { renderUserBlock } from './user.js'
+import {checkInDateValue} from './search-form.js';
+import {checkOutDateValue} from './search-form.js';
+// import { renderToast } from './lib.js'
+
+window.addEventListener('DOMContentLoaded', () => {
+  renderUserBlock('Wade Warren', 'img/avatar.png', 1)
+  renderSearchFormBlock(checkInDateValue, checkOutDateValue)
+  renderSearchStubBlock()
+  // renderToast(
+  //     {text: 'Это пример уведомления. Используйте его при необходимости', type: 'success'},
+  //     {name: 'Понял', handler: () => {console.log('Уведомление закрыто')}}
+  // )
+})

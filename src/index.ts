d6b2c973ccfab279a57aaf1b052af78a7b73bb6c
Mnishek <shavyrina.marina@mail.ru>
2022-02-11 12:22:55 +0300

    dz_3

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import {getUserData, getFavoritesAmount, renderUserBlock} from './user.js'
 import {checkInDateValue} from './search-form.js';
 import {checkOutDateValue} from './search-form.js';
 // import { renderToast } from './lib.js'
+import {getTodosByCount} from "./testAPI.js";
+import {getUser, getUserGeo} from "./testAPI.js";
+
 
 
 
@@ -19,6 +22,31 @@ const favoritesAmount = localStorage.getItem('favoritesAmount')
 
 
 window.addEventListener('DOMContentLoaded', () => {
+
+  getTodosByCount(7)
+    .then (todos => {
+      console.log(todos)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+
+  // getUser()
+  //   .then (user => {
+  //     console.log(user)
+  //   })
+  //   .catch((error) => {
+  //     console.error(error)
+  //   })
+  //
+  // getUserGeo()
+  //   .then (address => {
+  //     console.log(address.geo)
+  //   })
+  //   .catch((error) => {
+  //     console.error(error)
+  //   })
+
   renderUserBlock(getUserData(user),getFavoritesAmount (favoritesAmount))
   renderSearchFormBlock(checkInDateValue, checkOutDateValue)
   renderSearchStubBlock()
